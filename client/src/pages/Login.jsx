fc40bebd25b6c741b8501443e91da637fe9bfae0
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2025-01-22 14:03:07 +0545

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/v1/users/login",
+        "https://podcaster-server.vercel.app/api/v1/users/login",
         values,
         { withCredentials: true }
       );
